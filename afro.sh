c72007d0247ddacd955d40a687a9bc6b17a2ee8a
That1EthicalHacker <116861198+vel2006@users.noreply.github.com>
2024-05-31 18:23:37 -0500

    Update afro.sh

--- afro.sh
+++ afro.sh
@@ -61,3 +61,63 @@ UpdatePackage()
     echo "ERROR: $1 package does not exist or not connected to internet"
   fi
 }
+if [ $1 == "-h" || $1 == "--help" ]; then
+  echo "Afro is the custom (very bad) package manager for Afro Linux"
+  echo "It only works for custom scripts the dev has published on Github under the package's branch"
+  echo "Format to use is this:"
+  echo "afro <add/remove/update/version/find> <package>"
+  exit
+fi
+if [ $# != 2 ]; then
+  echo "ERROR: incorrect amount of arguments, correct format: (you can only do them one at a time)"
+  echo "afro <add/remove/update/version/find> <package>"
+  exit
+if [ $1 == "remove" ]; then
+  if [ $(PackageOnSystem $1) == 1 ]; then
+    RemovePackage $2
+  else
+    echo "ERROR: Package not installed"
+  fi
+elif [ $1 == "find" ]; then
+  if [ $(PackageExists) == 1 ]; then
+    if [ $(PackageOnSystem $1) == 1 ]; then
+      if [ $(SystemPackageVersion $1) < $(PackageVersion $1) ]; then
+        echo "Package exists and can be updated"
+      else [ $(SystemPackageVersion $1) == $(PackageVersion $1) ]; then
+        echo "Package exists and is up to date"
+      fi
+    else
+      echo "Package exists and can be downloaded"
+    fi
+  else
+    echo "ERROR: Package does not exists in the Afro Linux packages"
+  fi
+  exit
+elif [ $1 == "add" ]; then
+  if [ $(PackageExists) == 1 ]; then
+    if [ $(PackageOnSystem $1) == 1 ]; then
+      echo "ERROR: Package already installed"
+    else
+      DownloadPackage $1
+    fi
+  else
+    echo "ERROR: Package does not exist in the Afro Linux packages"
+  fi
+  exit
+elif [ $1 == "update" ]; then
+  if [ $(PackageExists) == 1 ]; then
+    UpdatePackage $1
+  else
+    echo "ERROR: Package does not exist in the Afro Linux packages"
+  fi
+  exit
+elif [ $1 == "version" ]; then
+  if [ $(PackageOnExists $1) ]; then
+    echo "$1 version: $(SystemPackageVersion $1)"
+  else
+    echo "ERROR: Package not installed"
+  fi
+  exit
+else
+  echo "ERROR: Incorrect arguments"
+fi
